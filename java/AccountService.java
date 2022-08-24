6963250f1a00cd857cc38a4dbb3e5dd9a1baba6f
rahulr0y <rahulkachappilly@gmail.com>
2022-08-24 17:13:44 +0530

    deleted

--- java/AccountService.java
+++
@@ -1,9 +0,0 @@
-
-public interface AccountService {
-
-    public void createAccount(Account account);
-    public Account searchByFirstName(String name);
-    public Account searchById(String id);
-    public boolean DeleteById(String id);
-    
-}
